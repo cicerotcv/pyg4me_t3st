e10a6252ff275a82bfef8d899f0c3a958d009591
cicerotcv <49568618+cicerotcv@users.noreply.github.com>
2019-04-23 11:03:46 -0300

    editei o sprite da nave e adicionei linhas de código

--- asteroids-base.py
+++ asteroids-base.py
@@ -20,15 +20,41 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
+# Classe Jogador que representa a nave
+class Player(pygame.sprite.Sprite):
+    
+    # Construtor da classe:
+    def __init__(self):
+        # Construtor da classe pai (Sprite):
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Carregando a imagem de fundo:
+        player_img = pygame.image.load(path.join(img_dir, "playerShip1_orange.png")).convert()
+        self.image = player_img
+        
+        # Diminuindo o tamanho da imagem:
+        self.image = pygame.transform.scale(player_img,(50,38))
+        
+        # Deixando a imagem transparente:
+        self.image.set_colorkey(BLACK)
+        
+        # Detalhes sobre o posicionamento:
+        self.rect = self.image.get_rect()
+        
+        # Centraliza embaixo da tela:
+        self.rect.centerx = WIDTH/2
+        self.rect.bottom = HEIGHT -  10
+
 # Inicialização do Pygame.
 pygame.init()
 pygame.mixer.init()
 
+        
 # Tamanho da tela.
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # Nome do jogo
-pygame.display.set_caption("Asteroids")
+pygame.display.set_caption("Bryan")
 
 # Variável para o ajuste de velocidade
 clock = pygame.time.Clock()
@@ -36,6 +62,14 @@ clock = pygame.time.Clock()
 # Carrega o fundo do jogo
 background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
 background_rect = background.get_rect()
+
+#  Cria uma nave. O construtor será chamado de automaticamente.
+player = Player()
+
+# Cria um grupo de sprites e adiciona a nave:
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
 
 # Comando para evitar travamentos.
 try:
@@ -57,6 +91,7 @@ try:
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
         screen.blit(background, background_rect)
+        all_sprites.draw(screen)
         
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
