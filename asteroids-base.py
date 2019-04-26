fd9b5ac42a760f2af202cb0c1171e14cf9744692
cicerotcv <49568618+cicerotcv@users.noreply.github.com>
2019-04-26 15:54:49 -0300

    não sei qual foi a ultima adicao

--- asteroids-base.py
+++ asteroids-base.py
@@ -20,6 +20,10 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
+# Inicialização do Pygame.
+pygame.init()
+pygame.mixer.init()
+
 # Classe Jogador que representa a nave
 class Player(pygame.sprite.Sprite):
     
@@ -44,10 +48,19 @@ class Player(pygame.sprite.Sprite):
         # Centraliza embaixo da tela:
         self.rect.centerx = WIDTH/2
         self.rect.bottom = HEIGHT -  10
-
-# Inicialização do Pygame.
-pygame.init()
-pygame.mixer.init()
+        
+        # Velocidade da nave:
+        self.speedx = 0
+        
+    # Método que atualiza a posição da navezinha:
+    def update(self):
+        self.rect.x += self.speedx
+        
+        # Mantem dentro da tela:
+        if self.rect.right >WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
 
         
 # Tamanho da tela.
@@ -87,7 +100,24 @@ try:
             # Verifica se foi fechado
             if event.type == pygame.QUIT:
                 running = False
-    
+            # Verifica se apertou alguma tecla:
+            if event.type == pygame.KEYDOWN:
+                # Dependendo da tecla, altera a velocidade:
+                if event.key == pygame.K_LEFT:
+                    player.speedx = -8
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 8
+            # Verifica se soltou alguma tecla:
+            if event.type == pygame.KEYUP:
+                # Dependendo da tecla:
+                if event.key == pygame.K_LEFT:
+                    player.speedx = 0
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 0
+        
+        # Depois de processar cada evento:
+        # Atualiza a acao de casa sprite:
+        all_sprites.update()
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
         screen.blit(background, background_rect)
@@ -95,6 +125,7 @@ try:
         
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
+        
         
 finally:
     pygame.quit()
